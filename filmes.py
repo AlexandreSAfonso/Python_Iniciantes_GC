d91e6fae7d7f9971e227f64aa38784f82e289c37
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-03-02 15:35:55 -0300

    Create filmes.py

---
+++ filmes.py
@@ -0,0 +1,11 @@
+### Variaveis
+
+NomeDoFilme = "Star Wars"
+AnoDoFilme = 219
+NotaDoFilme = 7.4
+FilmeDisponivel = True
+
+print(NomeDoFilme, AnoDoFilme, NotaDoFilme, FilmeDisponivel, sep='\n') 
+
+#sep= o que deve colocar como separador, '\n' separador de linha 
+#end= define o que deve ser impresso ao final de todos.
