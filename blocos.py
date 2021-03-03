c75ee363d36f32e35b2d98f002c6608cab455c5c
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-03-03 08:53:56 -0300

    Create blocos.py

---
+++ blocos.py
@@ -0,0 +1,30 @@
+#yaml
+#blooco de conteúdo dentro de indentamento
+
+#if Elif Else
+name = "Gustavo"
+age = 20
+gender = "male"
+
+
+# == igualdade
+# > ou >= 
+# < ou <=
+# and (todo verdadeiros)
+# or  (uma delas verdadeira)
+# is - compara de identidade ou tipo 
+
+if 18 >= age < 70:
+    print("voce pode dirigir")
+elif age < 70:
+    print("voce pode dirigir")
+else:
+    print("voce não pode dirigir")
+
+# while
+while age > 18
+
+# for
+friends =["eduardo", "Dan", 1]
+for friend in friends
+    print(friend)
